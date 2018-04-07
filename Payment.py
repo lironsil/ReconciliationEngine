ce9968cf3fd92835a4a25ee5f8b0a0a74003df57
lironsil <lironsilfin@gmail.com>
2018-04-07 11:54:14 +0300

    Add files via upload

--- Payment.py
+++ Payment.py
@@ -7,7 +7,6 @@ class Payment(object):
     def __init__(self, amount ,payment_reference ,payment_date):
        self.amount=amount
        self.payment_reference=payment_reference
-       
        self.payment_date=payment_date
        
 
